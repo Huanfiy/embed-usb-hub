7390c6f478d019b6f4d896b6396d9891eb311625
huan-homepcU22 <haunfly999@gmail.com>
2025-05-28 00:43:22 +0800

    [feat] usb hub can be identified

--- applications/main.c
+++ applications/main.c
@@ -7,22 +7,46 @@
  * Date           Author       Notes
  * 2025-05-20     RT-Thread    first version
  */
-
 #include <rtthread.h>
 
-#define DBG_TAG "main"
-#define DBG_LVL DBG_LOG
-#include <rtdbg.h>
+#include "app_gpio_drv.h"
+
+static void typec_irq_callback(void *args);
+static void typec_timer_callback(void *args);
+static rt_timer_t typec_timer;
 
 int main(void)
 {
-    int count = 1;
+    rt_pin_attach_irq(TYPEC_DETECT, PIN_IRQ_MODE_RISING, typec_irq_callback, (void *)TYPEC_DETECT);
+    rt_pin_irq_enable(TYPEC_DETECT, PIN_IRQ_ENABLE);
 
-    while (count++)
+    typec_timer = rt_timer_create("typec_timer", typec_timer_callback, NULL, 500,
+                                  RT_TIMER_FLAG_ONE_SHOT | RT_TIMER_FLAG_SOFT_TIMER);
+    if (typec_timer == RT_NULL)
     {
-        LOG_D("Hello RT-Thread!");
-        rt_thread_mdelay(1000);
+        rt_kprintf("typec_timer create failed!\n");
     }
 
+    app_gpio_ops.set_high(USB_EN);
+
+    while (1) { rt_thread_mdelay(1000); }
+
     return RT_EOK;
+}
+
+static void typec_irq_callback(void *args)
+{
+    RT_UNUSED(args);
+
+    rt_kprintf("typec_irq_callback\n");
+    app_gpio_ops.set_high(USB3_CONNECT);
+    rt_timer_start(typec_timer);
+}
+
+static void typec_timer_callback(void *args)
+{
+    RT_UNUSED(args);
+
+    rt_kprintf("typec_timer\n");
+    app_gpio_ops.set_high(USB2_CONNECT);
 }
